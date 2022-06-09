148d9742ecacab83a746d11ca67a26cefe1b8210
beryl-ajuoga <berrylajuoga@gmail.com>
2022-06-09 22:27:16 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,19 +1,42 @@
 class Account:
-    def __init__(self ,name ,number , balance , bank_name):
-        self.deposit = name
-        self.withdraw = number
-        self.withdraw = balance
-        self.withdraw = bank_name
-        
+    def __init__(self ,name ,acc_number):
+        self.name = name
+        self.acc_number =acc_number
+        self.balance = 0
+        self.deposits=[]
+        self.withdrawal=[]
 
-        
+      
+    def deposit(self , amount):
+        if amount <= 0:
+            return f"Deposit amount should be more than zero"
+        else:
+            self.balance +=amount
+            self.deposits.append(amount)
+            return f"You've deposited {amount}.Your new balance is {self.balance}" ,self.deposits
 
-    def deposit(self,amount):
-        self.balance+=amount
-        return f"hello your current deposit is { self.deposit}"
 
     def withdraw(self,amount):
-        self.balance+=amount
-        return f"you'll be charged { self.withdraw} as the withdrawal amount "
+        if amount > self.balance:
+            return f"Your balance is {self.balance}.You cannot withdraw the {amount}"
+        elif amount <=0:
+            return f"Amount must be greater than zero"
+        else:
+            self.balance -=amount
+            self.withdrawal.append(amount)
+            return f"You have withdrawn {amount} your balance is {self.balance}",self.withdrawal   
+
+    def deposit_statement(self) :
+        print(*self.deposits,sep="\n")
+
+    def withdrawal_statement(self) :
+        print(*self.deposits,sep="\n")
+                   
+        
+
+
+
+
+
 
    
